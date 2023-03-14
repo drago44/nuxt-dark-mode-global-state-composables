2fdc1a35453637f3eca864f6703e762781005584
Alex <oleksander.kucherenko.work@gmail.com>
2023-03-14 16:45:19 +0200

    add landing page

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <Nav />
+    <Heading />
+    <Cards />
   </div>
 </template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: Oxygen, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=500, initial-scale=1',
+      title: 'Dark mode',
+      meta: [{ name: 'description', content: 'My amazing site.' }],
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Oxygen&display=swap',
+        },
+      ],
+    },
+  },
+});
